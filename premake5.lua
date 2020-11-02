45dff78b75d3e5965e72cc99167fe7aabea1ce1f
Hector Peeters <hector.peeters@gmail.com>
2020-11-02 11:29:28 +0100

    Initial commit

---
+++ premake5.lua
@@ -0,0 +1,41 @@
+workspace "raytracer"
+    configurations { "Debug", "Release" }
+    startproject "OpenGLBoilerPlate"
+
+    flags { "MultiProcessorCompile" }
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "RELEASE" }
+        optimize "Speed"
+        flags { "LinkTimeOptimization" }
+
+project "raytracer"
+    kind "ConsoleApp"
+    language "C"
+    architecture "x86_64"
+
+    targetdir "bin/%{cfg.buildcfg}"
+    objdir "obj/%{cfg.buildcfg}"
+
+    includedirs { "include/", "libs/glad/include/", "libs/glfw/include/", "libs/glm/", "libs/imgui/", "libs/imgui/examples" }
+    
+    files { "src/*.c" }
+
+    links { "GLFW", "GLM", "GLAD", "ImGui" }
+
+    filter "system:linux"
+        links { "dl", "pthread" }
+
+        defines { "_X11" }
+
+    filter "system:windows"
+        defines { "_WINDOWS" }
+
+include "libs/glfw.lua"
+include "libs/glad.lua"
+include "libs/glm.lua"
+include "libs/imgui.lua"
