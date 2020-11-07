54a1fde736f2f5860e58e1ed2d19e36f976f3330
Hector Peeters <hector.peeters@gmail.com>
2020-11-07 12:15:32 +0100

    Add vec3 tests

---
+++ tests/test_vec3f.cpp
@@ -0,0 +1,197 @@
+#include <catch.hpp>
+
+#include "core.h"
+#include "vec3.h"
+
+TEST_CASE("vec3f constructor sets all fields", "[vec3f]") {
+  vec3f vec = vec3f(1.0f, 2.0f, 3.0f);
+
+  REQUIRE(APPROX_EQ(vec.x, 1.0f));
+  REQUIRE(APPROX_EQ(vec.y, 2.0f));
+  REQUIRE(APPROX_EQ(vec.z, 3.0f));
+}
+
+TEST_CASE("vec3f length", "[vec3f]") {
+  vec3f vec = vec3f(1.0f, 2.0f, 3.0f);
+  REQUIRE(APPROX_EQ(vec.length(), 3.741657f));
+}
+
+TEST_CASE("vec3f length_sqrt", "[vec3f]") {
+  vec3f vec = vec3f(1.0f, 2.0f, 3.0f);
+  REQUIRE(APPROX_EQ(vec.length_sqrt(), 14.0f));
+}
+
+TEST_CASE("vec3f normalized", "[vec3f]") {
+  vec3f vec = vec3f(1.0f, 2.0f, 3.0f);
+  vec3f normalized = vec.normalized();
+
+  REQUIRE(APPROX_EQ(normalized.length(), 1.0f));
+
+  vec3f scalar = vec / normalized;
+  REQUIRE(APPROX_EQ(scalar.x, scalar.y));
+  REQUIRE(APPROX_EQ(scalar.y, scalar.z));
+  REQUIRE(APPROX_EQ(scalar.z, scalar.x));
+}
+
+TEST_CASE("vec3f normalize", "[vec3f]") {
+  vec3f vec = vec3f(1.0f, 2.0f, 3.0f);
+  vec.normalize();
+
+  REQUIRE(APPROX_EQ(vec.length(), 1.0f));
+
+  vec3f scalar = vec3f(1.0f, 2.0f, 3.0f) / vec;
+  REQUIRE(APPROX_EQ(scalar.x, scalar.y));
+  REQUIRE(APPROX_EQ(scalar.y, scalar.z));
+  REQUIRE(APPROX_EQ(scalar.z, scalar.x));
+}
+
+TEST_CASE("vec3f dot", "[vec3f]") {
+  vec3f a = vec3f(1.0f, 0.0f, 0.0f);
+  vec3f b = vec3f(0.0f, 1.0f, 0.0f);
+  vec3f c = vec3f(1.0f, 2.0f, 3.0f);
+
+  REQUIRE(APPROX_EQ(a.dot(b), 0.0f));
+  REQUIRE(APPROX_EQ(c.dot(c), c.length_sqrt()));
+  REQUIRE(APPROX_EQ(a.dot(c), 1.0f));
+  REQUIRE(APPROX_EQ(b.dot(c), 2.0f));
+}
+
+TEST_CASE("vec3f cross", "[vec3f]") {
+  vec3f a = vec3f(1.0f, 2.0f, 3.0f);
+  vec3f b = a.cross(vec3f(1.0f, 0.0, 0.0));
+  vec3f c = a.cross(b);
+
+  REQUIRE(APPROX_EQ(a.dot(b), 0.0f));
+  REQUIRE(APPROX_EQ(b.dot(c), 0.0f));
+  REQUIRE(APPROX_EQ(c.dot(a), 0.0f));
+}
+
+TEST_CASE("vec3f equals operator", "[vec3f]") {
+  vec3f a = vec3f(1.4f, -5.7f, 3.0f);
+  vec3f b = vec3f(1.4f, -5.7f, 3.0f);
+  REQUIRE(a == b);
+}
+
+// vec3f vec3f operations
+
+TEST_CASE("vec3f add vec3f", "[vec3f]") {
+  vec3f a = vec3f(1.0f, 2.0f, 3.0f);
+  vec3f b = vec3f(3.0f, 2.0f, 1.0f);
+  vec3f res = a + b;
+
+  REQUIRE(res == vec3f(4.0f, 4.0f, 4.0f));
+}
+
+TEST_CASE("vec3f sub vec3f", "[vec3f]") {
+  vec3f a = vec3f(1.0f, 2.0f, 3.0f);
+  vec3f b = vec3f(3.0f, 2.0f, 1.0f);
+  vec3f res = a - b;
+
+  REQUIRE(res == vec3f(-2.0f, 0.0f, 2.0f));
+}
+
+TEST_CASE("vec3f mul vec3f", "[vec3f]") {
+  vec3f a = vec3f(1.0f, 2.0f, 3.0f);
+  vec3f b = vec3f(3.0f, 2.0f, 1.0f);
+  vec3f res = a * b;
+
+  REQUIRE(res == vec3f(3.0f, 4.0f, 3.0f));
+}
+
+TEST_CASE("vec3f div vec3f", "[vec3f]") {
+  vec3f a = vec3f(1.0f, 2.0f, 3.0f);
+  vec3f b = vec3f(3.0f, 2.0f, 1.0f);
+  vec3f res = a / b;
+
+  REQUIRE(res == vec3f(0.33333f, 1.0f, 3.0f));
+}
+
+// vec3f f32 operations
+
+TEST_CASE("vec3f add f32", "[vec3f]") {
+  vec3f a = vec3f(1.0f, 2.0f, 3.0f);
+  vec3f res = a + 2.0f;
+
+  REQUIRE(res == vec3f(3.0f, 4.0f, 5.0f));
+}
+
+TEST_CASE("vec3f sub f32", "[vec3f]") {
+  vec3f a = vec3f(1.0f, 2.0f, 3.0f);
+  vec3f res = a - 2.0f;
+
+  REQUIRE(res == vec3f(-1.0f, 0.0f, 1.0f));
+}
+
+TEST_CASE("vec3f mul f32", "[vec3f]") {
+  vec3f a = vec3f(1.0f, 2.0f, 3.0f);
+  vec3f res = a * 2.0f;
+
+  REQUIRE(res == vec3f(2.0f, 4.0f, 6.0f));
+}
+
+TEST_CASE("vec3f div f32", "[vec3f]") {
+  vec3f a = vec3f(1.0f, 2.0f, 3.0f);
+  vec3f res = a / 2.0f;
+
+  REQUIRE(res == vec3f(0.5f, 1.0f, 1.5f));
+}
+
+// vec3f vec3f assignment operators
+
+TEST_CASE("vec3f add assign vec3f", "[vec3f]") {
+  vec3f a = vec3f(1.0f, 2.0f, 3.0f);
+  a += vec3f(3.0f, 2.0f, 1.0f);
+
+  REQUIRE(a == vec3f(4.0f, 4.0f, 4.0f));
+}
+
+TEST_CASE("vec3f sub assign vec3f", "[vec3f]") {
+  vec3f a = vec3f(1.0f, 2.0f, 3.0f);
+  a -= vec3f(3.0f, 2.0f, 1.0f);
+
+  REQUIRE(a == vec3f(-2.0f, 0.0f, 2.0f));
+}
+
+TEST_CASE("vec3f mul assign vec3f", "[vec3f]") {
+  vec3f a = vec3f(1.0f, 2.0f, 3.0f);
+  a *= vec3f(3.0f, 2.0f, 1.0f);
+
+  REQUIRE(a == vec3f(3.0f, 4.0f, 3.0f));
+}
+
+TEST_CASE("vec3f div assign vec3f", "[vec3f]") {
+  vec3f a = vec3f(1.0f, 2.0f, 3.0f);
+  a /= vec3f(3.0f, 2.0f, 1.0f);
+
+  REQUIRE(a == vec3f(0.33333333f, 1.0f, 3.0f));
+}
+
+// vec3f float assignment operators
+
+TEST_CASE("vec3f add assign f32", "[vec3f]") {
+  vec3f a = vec3f(1.0f, 2.0f, 3.0f);
+  a += 2.0f;
+
+  REQUIRE(a == vec3f(3.0f, 4.0f, 5.0f));
+}
+
+TEST_CASE("vec3f sub assign f32", "[vec3f]") {
+  vec3f a = vec3f(1.0f, 2.0f, 3.0f);
+  a -= 2.0f;
+
+  REQUIRE(a == vec3f(-1.0f, 0.0f, 1.0f));
+}
+
+TEST_CASE("vec3f mul assign f32", "[vec3f]") {
+  vec3f a = vec3f(1.0f, 2.0f, 3.0f);
+  a *= 2.0f;
+
+  REQUIRE(a == vec3f(2.0f, 4.0f, 6.0f));
+}
+
+TEST_CASE("vec3f div assign f32", "[vec3f]") {
+  vec3f a = vec3f(1.0f, 2.0f, 3.0f);
+  a /= 2.0f;
+
+  REQUIRE(a == vec3f(0.5f, 1.0f, 1.5f));
+}
---
+++ tests/test_vec3i.cpp
@@ -0,0 +1,172 @@
+#include <catch.hpp>
+
+#include "vec3.h"
+
+TEST_CASE("vec3i constructor sets all fields", "[vec3i]") {
+  vec3i vec = vec3i(1, 2, 3);
+
+  REQUIRE(vec.x == 1);
+  REQUIRE(vec.y == 2);
+  REQUIRE(vec.z == 3);
+}
+
+TEST_CASE("vec3i length", "[vec3i]") {
+  vec3i vec = vec3i(1, 2, 3);
+  REQUIRE(APPROX_EQ(vec.length(), 3.741657f));
+}
+
+TEST_CASE("vec3i length_sqrt", "[vec3i]") {
+  vec3i vec = vec3i(1, 2, 3);
+  REQUIRE(vec.length_sqrt() == 14);
+}
+
+TEST_CASE("vec3i dot", "[vec3i]") {
+  vec3i a = vec3i(1, 0, 0);
+  vec3i b = vec3i(0, 1, 0);
+  vec3i c = vec3i(1, 2, 3);
+
+  REQUIRE(a.dot(b) == 0);
+  REQUIRE(c.dot(c) == c.length_sqrt());
+  REQUIRE(a.dot(c) == 1);
+  REQUIRE(b.dot(c) == 2);
+}
+
+TEST_CASE("vec3i cross", "[vec3i]") {
+  vec3i a = vec3i(1, 2, 3);
+  vec3i b = a.cross(vec3i(1, 0.0, 0.0));
+  vec3i c = a.cross(b);
+
+  REQUIRE(a.dot(b) == 0);
+  REQUIRE(b.dot(c) == 0);
+  REQUIRE(c.dot(a) == 0);
+}
+
+TEST_CASE("vec3i equals operator", "[vec3i]") {
+  vec3i a = vec3i(2, -4, 3);
+  vec3i b = vec3i(2, -4, 3);
+  REQUIRE(a == b);
+}
+
+// vec3i vec3i operations
+
+TEST_CASE("vec3i add vec3i", "[vec3i]") {
+  vec3i a = vec3i(1, 2, 3);
+  vec3i b = vec3i(3, 2, 1);
+  vec3i res = a + b;
+
+  REQUIRE(res == vec3i(4, 4, 4));
+}
+
+TEST_CASE("vec3i sub vec3i", "[vec3i]") {
+  vec3i a = vec3i(1, 2, 3);
+  vec3i b = vec3i(3, 2, 1);
+  vec3i res = a - b;
+
+  REQUIRE(res == vec3i(-2, 0, 2));
+}
+
+TEST_CASE("vec3i mul vec3i", "[vec3i]") {
+  vec3i a = vec3i(1, 2, 3);
+  vec3i b = vec3i(3, 2, 1);
+  vec3i res = a * b;
+
+  REQUIRE(res == vec3i(3, 4, 3));
+}
+
+TEST_CASE("vec3i div vec3i", "[vec3i]") {
+  vec3i a = vec3i(1, 2, 3);
+  vec3i b = vec3i(3, 2, 1);
+  vec3i res = a / b;
+
+  REQUIRE(res == vec3i(0, 1, 3));
+}
+
+// vec3i f32 operations
+
+TEST_CASE("vec3i add i32", "[vec3i]") {
+  vec3i a = vec3i(1, 2, 3);
+  vec3i res = a + 2;
+
+  REQUIRE(res == vec3i(3, 4, 5));
+}
+
+TEST_CASE("vec3i sub i32", "[vec3i]") {
+  vec3i a = vec3i(1, 2, 3);
+  vec3i res = a - 2;
+
+  REQUIRE(res == vec3i(-1, 0, 1));
+}
+
+TEST_CASE("vec3i mul i32", "[vec3i]") {
+  vec3i a = vec3i(1, 2, 3);
+  vec3i res = a * 2;
+
+  REQUIRE(res == vec3i(2, 4, 6));
+}
+
+TEST_CASE("vec3i div i32", "[vec3i]") {
+  vec3i a = vec3i(2, 4, 8);
+  vec3i res = a / 2;
+
+  REQUIRE(res == vec3i(1, 2, 4));
+}
+
+// vec3i vec3i assignment operators
+
+TEST_CASE("vec3i add assign vec3i", "[vec3i]") {
+  vec3i a = vec3i(1, 2, 3);
+  a += vec3i(3, 2, 1);
+
+  REQUIRE(a == vec3i(4, 4, 4));
+}
+
+TEST_CASE("vec3i sub assign vec3i", "[vec3i]") {
+  vec3i a = vec3i(1, 2, 3);
+  a -= vec3i(3, 2, 1);
+
+  REQUIRE(a == vec3i(-2, 0, 2));
+}
+
+TEST_CASE("vec3i mul assign vec3i", "[vec3i]") {
+  vec3i a = vec3i(1, 2, 3);
+  a *= vec3i(3, 2, 1);
+
+  REQUIRE(a == vec3i(3, 4, 3));
+}
+
+TEST_CASE("vec3i div assign vec3i", "[vec3i]") {
+  vec3i a = vec3i(1, 2, 3);
+  a /= vec3i(3, 2, 1);
+
+  REQUIRE(a == vec3i(0, 1, 3));
+}
+
+// vec3i float assignment operators
+
+TEST_CASE("vec3i add assign i32", "[vec3i]") {
+  vec3i a = vec3i(1, 2, 3);
+  a += 2;
+
+  REQUIRE(a == vec3i(3, 4, 5));
+}
+
+TEST_CASE("vec3i sub assign i32", "[vec3i]") {
+  vec3i a = vec3i(1, 2, 3);
+  a -= 2;
+
+  REQUIRE(a == vec3i(-1, 0, 1));
+}
+
+TEST_CASE("vec3i mul assign i32", "[vec3i]") {
+  vec3i a = vec3i(1, 2, 3);
+  a *= 2;
+
+  REQUIRE(a == vec3i(2, 4, 6));
+}
+
+TEST_CASE("vec3i div assign i32", "[vec3i]") {
+  vec3i a = vec3i(1, 2, 3);
+  a /= 2;
+
+  REQUIRE(a == vec3i(0, 1, 1));
+}
