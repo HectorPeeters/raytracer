d526d55d8b48192b7b432a7dcc0f116af03721d6
Hector Peeters <hector.peeters@gmail.com>
2020-11-07 13:55:23 +0100

    Add tests for vec4

---
+++ tests/test_vec4f.cpp
@@ -0,0 +1,188 @@
+#include <catch.hpp>
+
+#include "core.h"
+#include "vec4.h"
+
+TEST_CASE("vec4f constructor sets all fields", "[vec4f]") {
+  vec4f vec = vec4f(1.0f, 2.0f, 3.0f, 4.0);
+
+  REQUIRE(APPROX_EQ(vec.x, 1.0f));
+  REQUIRE(APPROX_EQ(vec.y, 2.0f));
+  REQUIRE(APPROX_EQ(vec.z, 3.0f));
+  REQUIRE(APPROX_EQ(vec.w, 4.0f));
+}
+
+TEST_CASE("vec4f length", "[vec4f]") {
+  vec4f vec = vec4f(1.0f, 2.0f, 3.0f, 4.0f);
+  REQUIRE(APPROX_EQ(vec.length(), 5.477226f));
+}
+
+TEST_CASE("vec4f length_sqrt", "[vec4f]") {
+  vec4f vec = vec4f(1.0f, 2.0f, 3.0f, 4.0f);
+  REQUIRE(APPROX_EQ(vec.length_sqrt(), 30.0f));
+}
+
+TEST_CASE("vec4f normalized", "[vec4f]") {
+  vec4f vec = vec4f(1.0f, 2.0f, 3.0f, 4.0f);
+  vec4f normalized = vec.normalized();
+
+  REQUIRE(APPROX_EQ(normalized.length(), 1.0f));
+
+  vec4f scalar = vec / normalized;
+  REQUIRE(APPROX_EQ(scalar.x, scalar.y));
+  REQUIRE(APPROX_EQ(scalar.y, scalar.z));
+  REQUIRE(APPROX_EQ(scalar.z, scalar.x));
+}
+
+TEST_CASE("vec4f normalize", "[vec4f]") {
+  vec4f vec = vec4f(1.0f, 2.0f, 3.0f, 4.0f);
+  vec.normalize();
+
+  REQUIRE(APPROX_EQ(vec.length(), 1.0f));
+
+  vec4f scalar = vec4f(1.0f, 2.0f, 3.0f, 4.0f) / vec;
+  REQUIRE(APPROX_EQ(scalar.x, scalar.y));
+  REQUIRE(APPROX_EQ(scalar.y, scalar.z));
+  REQUIRE(APPROX_EQ(scalar.z, scalar.x));
+}
+
+TEST_CASE("vec4f dot", "[vec4f]") {
+  vec4f a = vec4f(1.0f, 0.0f, 0.0f, 0.0f);
+  vec4f b = vec4f(0.0f, 1.0f, 0.0f, 0.0f);
+  vec4f c = vec4f(1.0f, 2.0f, 3.0f, 4.0f);
+
+  REQUIRE(APPROX_EQ(a.dot(b), 0.0f));
+  REQUIRE(APPROX_EQ(c.dot(c), c.length_sqrt()));
+  REQUIRE(APPROX_EQ(a.dot(c), 1.0f));
+  REQUIRE(APPROX_EQ(b.dot(c), 2.0f));
+}
+
+TEST_CASE("vec4f equals operator", "[vec4f]") {
+  vec4f a = vec4f(1.4f, -5.7f, 3.0f, -4.94f);
+  vec4f b = vec4f(1.4f, -5.7f, 3.0f, -4.94f);
+  REQUIRE(a == b);
+}
+
+// vec4f vec4f operations
+
+TEST_CASE("vec4f add vec4f", "[vec4f]") {
+  vec4f a = vec4f(1.0f, 2.0f, 3.0f, 4.0f);
+  vec4f b = vec4f(4.0f, 3.0f, 2.0f, 1.0f);
+  vec4f res = a + b;
+
+  REQUIRE(res == vec4f(5.0f, 5.0f, 5.0f, 5.0f));
+}
+
+TEST_CASE("vec4f sub vec4f", "[vec4f]") {
+  vec4f a = vec4f(1.0f, 2.0f, 3.0f, 4.0f);
+  vec4f b = vec4f(4.0f, 3.0f, 2.0f, 1.0f);
+  vec4f res = a - b;
+
+  REQUIRE(res == vec4f(-3.0f, -1.0f, 1.0f, 3.0f));
+}
+
+TEST_CASE("vec4f mul vec4f", "[vec4f]") {
+  vec4f a = vec4f(1.0f, 2.0f, 3.0f, 4.0f);
+  vec4f b = vec4f(4.0f, 3.0f, 2.0f, 1.0f);
+  vec4f res = a * b;
+
+  REQUIRE(res == vec4f(4.0f, 6.0f, 6.0f, 4.0f));
+}
+
+TEST_CASE("vec4f div vec4f", "[vec4f]") {
+  vec4f a = vec4f(1.0f, 2.0f, 3.0f, 4.0f);
+  vec4f b = vec4f(4.0f, 3.0f, 2.0f, 1.0f);
+  vec4f res = a / b;
+
+  REQUIRE(res == vec4f(0.25f, 0.66666666f, 1.5f, 4.0f));
+}
+
+// vec4f f32 operations
+
+TEST_CASE("vec4f add f32", "[vec4f]") {
+  vec4f a = vec4f(1.0f, 2.0f, 3.0f, 4.0f);
+  vec4f res = a + 2.0f;
+
+  REQUIRE(res == vec4f(3.0f, 4.0f, 5.0f, 6.0f));
+}
+
+TEST_CASE("vec4f sub f32", "[vec4f]") {
+  vec4f a = vec4f(1.0f, 2.0f, 3.0f, 4.0f);
+  vec4f res = a - 2.0f;
+
+  REQUIRE(res == vec4f(-1.0f, 0.0f, 1.0f, 2.0f));
+}
+
+TEST_CASE("vec4f mul f32", "[vec4f]") {
+  vec4f a = vec4f(1.0f, 2.0f, 3.0f, 4.0f);
+  vec4f res = a * 2.0f;
+
+  REQUIRE(res == vec4f(2.0f, 4.0f, 6.0f, 8.0f));
+}
+
+TEST_CASE("vec4f div f32", "[vec4f]") {
+  vec4f a = vec4f(1.0f, 2.0f, 3.0f, 4.0f);
+  vec4f res = a / 2.0f;
+
+  REQUIRE(res == vec4f(0.5f, 1.0f, 1.5f, 2.0f));
+}
+
+// vec4f vec4f assignment operators
+
+TEST_CASE("vec4f add assign vec4f", "[vec4f]") {
+  vec4f a = vec4f(1.0f, 2.0f, 3.0f, 4.0f);
+  a += vec4f(4.0f, 3.0f, 2.0f, 1.0f);
+
+  REQUIRE(a == vec4f(5.0f, 5.0f, 5.0f, 5.0f));
+}
+
+TEST_CASE("vec4f sub assign vec4f", "[vec4f]") {
+  vec4f a = vec4f(1.0f, 2.0f, 3.0f, 4.0f);
+  a -= vec4f(4.0f, 3.0f, 2.0f, 1.0f);
+
+  REQUIRE(a == vec4f(-3.0f, -1.0f, 1.0f, 3.0f));
+}
+
+TEST_CASE("vec4f mul assign vec4f", "[vec4f]") {
+  vec4f a = vec4f(1.0f, 2.0f, 3.0f, 4.0f);
+  a *= vec4f(4.0f, 3.0f, 2.0f, 1.0f);
+
+  REQUIRE(a == vec4f(4.0f, 6.0f, 6.0f, 4.0f));
+}
+
+TEST_CASE("vec4f div assign vec4f", "[vec4f]") {
+  vec4f a = vec4f(1.0f, 2.0f, 3.0f, 4.0f);
+  a /= vec4f(4.0f, 3.0f, 2.0f, 1.0f);
+
+  REQUIRE(a == vec4f(0.25f, 0.666666f, 1.5f, 4.0f));
+}
+
+// vec4f f32 assignment operators
+
+TEST_CASE("vec4f add assign f32", "[vec4f]") {
+  vec4f a = vec4f(1.0f, 2.0f, 3.0f, 4.0f);
+  a += 2.0f;
+
+  REQUIRE(a == vec4f(3.0f, 4.0f, 5.0f, 6.0f));
+}
+
+TEST_CASE("vec4f sub assign f32", "[vec4f]") {
+  vec4f a = vec4f(1.0f, 2.0f, 3.0f, 4.0f);
+  a -= 2.0f;
+
+  REQUIRE(a == vec4f(-1.0f, 0.0f, 1.0f, 2.0f));
+}
+
+TEST_CASE("vec4f mul assign f32", "[vec4f]") {
+  vec4f a = vec4f(1.0f, 2.0f, 3.0f, 4.0f);
+  a *= 2.0f;
+
+  REQUIRE(a == vec4f(2.0f, 4.0f, 6.0f, 8.0f));
+}
+
+TEST_CASE("vec4f div assign f32", "[vec4f]") {
+  vec4f a = vec4f(1.0f, 2.0f, 3.0f, 4.0f);
+  a /= 2.0f;
+
+  REQUIRE(a == vec4f(0.5f, 1.0f, 1.5f, 2.0f));
+}
---
+++ tests/test_vec4i.cpp
@@ -0,0 +1,163 @@
+#include <catch.hpp>
+
+#include "core.h"
+#include "vec4.h"
+
+TEST_CASE("vec4i constructor sets all fields", "[vec4i]") {
+  vec4i vec = vec4i(1, 2, 3, 4.0);
+
+  REQUIRE(APPROX_EQ(vec.x, 1));
+  REQUIRE(APPROX_EQ(vec.y, 2));
+  REQUIRE(APPROX_EQ(vec.z, 3));
+  REQUIRE(APPROX_EQ(vec.w, 4));
+}
+
+TEST_CASE("vec4i length", "[vec4i]") {
+  vec4i vec = vec4i(1, 2, 3, 4);
+  REQUIRE(APPROX_EQ(vec.length(), 5.477226f));
+}
+
+TEST_CASE("vec4i length_sqrt", "[vec4i]") {
+  vec4i vec = vec4i(1, 2, 3, 4);
+  REQUIRE(APPROX_EQ(vec.length_sqrt(), 30));
+}
+TEST_CASE("vec4i dot", "[vec4i]") {
+  vec4i a = vec4i(1, 0, 0, 0);
+  vec4i b = vec4i(0, 1, 0, 0);
+  vec4i c = vec4i(1, 2, 3, 4);
+
+  REQUIRE(APPROX_EQ(a.dot(b), 0));
+  REQUIRE(APPROX_EQ(c.dot(c), c.length_sqrt()));
+  REQUIRE(APPROX_EQ(a.dot(c), 1));
+  REQUIRE(APPROX_EQ(b.dot(c), 2));
+}
+
+TEST_CASE("vec4i equals operator", "[vec4i]") {
+  vec4i a = vec4i(1, -5, 3, -4);
+  vec4i b = vec4i(1, -5, 3, -4);
+  REQUIRE(a == b);
+}
+
+// vec4i vec4i operations
+
+TEST_CASE("vec4i add vec4i", "[vec4i]") {
+  vec4i a = vec4i(1, 2, 3, 4);
+  vec4i b = vec4i(4, 3, 2, 1);
+  vec4i res = a + b;
+
+  REQUIRE(res == vec4i(5, 5, 5, 5));
+}
+
+TEST_CASE("vec4i sub vec4i", "[vec4i]") {
+  vec4i a = vec4i(1, 2, 3, 4);
+  vec4i b = vec4i(4, 3, 2, 1);
+  vec4i res = a - b;
+
+  REQUIRE(res == vec4i(-3, -1, 1, 3));
+}
+
+TEST_CASE("vec4i mul vec4i", "[vec4i]") {
+  vec4i a = vec4i(1, 2, 3, 4);
+  vec4i b = vec4i(4, 3, 2, 1);
+  vec4i res = a * b;
+
+  REQUIRE(res == vec4i(4, 6, 6, 4));
+}
+
+TEST_CASE("vec4i div vec4i", "[vec4i]") {
+  vec4i a = vec4i(1, 2, 3, 4);
+  vec4i b = vec4i(4, 3, 2, 1);
+  vec4i res = a / b;
+
+  REQUIRE(res == vec4i(0, 0, 1, 4));
+}
+
+// vec4i i32 operations
+
+TEST_CASE("vec4i add i32", "[vec4i]") {
+  vec4i a = vec4i(1, 2, 3, 4);
+  vec4i res = a + 2;
+
+  REQUIRE(res == vec4i(3, 4, 5, 6));
+}
+
+TEST_CASE("vec4i sub i32", "[vec4i]") {
+  vec4i a = vec4i(1, 2, 3, 4);
+  vec4i res = a - 2;
+
+  REQUIRE(res == vec4i(-1, 0, 1, 2));
+}
+
+TEST_CASE("vec4i mul i32", "[vec4i]") {
+  vec4i a = vec4i(1, 2, 3, 4);
+  vec4i res = a * 2;
+
+  REQUIRE(res == vec4i(2, 4, 6, 8));
+}
+
+TEST_CASE("vec4i div i32", "[vec4i]") {
+  vec4i a = vec4i(1, 2, 3, 4);
+  vec4i res = a / 2;
+
+  REQUIRE(res == vec4i(0, 1, 1, 2));
+}
+
+// vec4i vec4i assignment operators
+
+TEST_CASE("vec4i add assign vec4i", "[vec4i]") {
+  vec4i a = vec4i(1, 2, 3, 4);
+  a += vec4i(4, 3, 2, 1);
+
+  REQUIRE(a == vec4i(5, 5, 5, 5));
+}
+
+TEST_CASE("vec4i sub assign vec4i", "[vec4i]") {
+  vec4i a = vec4i(1, 2, 3, 4);
+  a -= vec4i(4, 3, 2, 1);
+
+  REQUIRE(a == vec4i(-3, -1, 1, 3));
+}
+
+TEST_CASE("vec4i mul assign vec4i", "[vec4i]") {
+  vec4i a = vec4i(1, 2, 3, 4);
+  a *= vec4i(4, 3, 2, 1);
+
+  REQUIRE(a == vec4i(4, 6, 6, 4));
+}
+
+TEST_CASE("vec4i div assign vec4i", "[vec4i]") {
+  vec4i a = vec4i(1, 2, 3, 4);
+  a /= vec4i(4, 3, 2, 1);
+
+  REQUIRE(a == vec4i(0, 0, 1, 4));
+}
+
+// vec4i float assignment operators
+
+TEST_CASE("vec4i add assign i32", "[vec4i]") {
+  vec4i a = vec4i(1, 2, 3, 4);
+  a += 2;
+
+  REQUIRE(a == vec4i(3, 4, 5, 6));
+}
+
+TEST_CASE("vec4i sub assign i32", "[vec4i]") {
+  vec4i a = vec4i(1, 2, 3, 4);
+  a -= 2;
+
+  REQUIRE(a == vec4i(-1, 0, 1, 2));
+}
+
+TEST_CASE("vec4i mul assign i32", "[vec4i]") {
+  vec4i a = vec4i(1, 2, 3, 4);
+  a *= 2;
+
+  REQUIRE(a == vec4i(2, 4, 6, 8));
+}
+
+TEST_CASE("vec4i div assign i32", "[vec4i]") {
+  vec4i a = vec4i(1, 2, 3, 4);
+  a /= 2;
+
+  REQUIRE(a == vec4i(0, 1, 1, 2));
+}
