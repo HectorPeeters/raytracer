3b78d24502b8c38a622b2d3f1c93f128c0413076
Hector Peeters <hector.peeters@gmail.com>
2020-11-07 12:32:55 +0100

    Add vec2 tests

---
+++ tests/test_vec2f.cpp
@@ -0,0 +1,182 @@
+#include <catch.hpp>
+
+#include "core.h"
+#include "vec2.h"
+
+TEST_CASE("vec2f constructor sets all fields", "[vec2f]") {
+  vec2f vec = vec2f(1.0f, 2.0f);
+
+  REQUIRE(APPROX_EQ(vec.x, 1.0f));
+  REQUIRE(APPROX_EQ(vec.y, 2.0f));
+}
+
+TEST_CASE("vec2f length", "[vec2f]") {
+  vec2f vec = vec2f(1.0f, 2.0f);
+  REQUIRE(APPROX_EQ(vec.length(), 2.236068f));
+}
+
+TEST_CASE("vec2f length_sqrt", "[vec2f]") {
+  vec2f vec = vec2f(1.0f, 2.0f);
+  REQUIRE(APPROX_EQ(vec.length_sqrt(), 5.0f));
+}
+
+TEST_CASE("vec2f normalized", "[vec2f]") {
+  vec2f vec = vec2f(1.0f, 2.0f);
+  vec2f normalized = vec.normalized();
+
+  REQUIRE(APPROX_EQ(normalized.length(), 1.0f));
+
+  vec2f scalar = vec / normalized;
+  REQUIRE(APPROX_EQ(scalar.x, scalar.y));
+}
+
+TEST_CASE("vec2f normalize", "[vec2f]") {
+  vec2f vec = vec2f(1.0f, 2.0f);
+  vec.normalize();
+
+  REQUIRE(APPROX_EQ(vec.length(), 1.0f));
+
+  vec2f scalar = vec2f(1.0f, 2.0f) / vec;
+  REQUIRE(APPROX_EQ(scalar.x, scalar.y));
+}
+
+TEST_CASE("vec2f dot", "[vec2f]") {
+  vec2f a = vec2f(1.0f, 0.0f);
+  vec2f b = vec2f(0.0f, 1.0f);
+  vec2f c = vec2f(1.0f, 2.0f);
+
+  REQUIRE(APPROX_EQ(a.dot(b), 0.0f));
+  REQUIRE(APPROX_EQ(c.dot(c), c.length_sqrt()));
+  REQUIRE(APPROX_EQ(a.dot(c), 1.0f));
+  REQUIRE(APPROX_EQ(b.dot(c), 2.0f));
+}
+
+TEST_CASE("vec2f equals operator", "[vec2f]") {
+  vec2f a = vec2f(1.4f, -5.7f);
+  vec2f b = vec2f(1.4f, -5.7f);
+  REQUIRE(a == b);
+}
+
+// vec2f vec2f operations
+
+TEST_CASE("vec2f add vec2f", "[vec2f]") {
+  vec2f a = vec2f(1.0f, 2.0f);
+  vec2f b = vec2f(3.0f, 2.0f);
+  vec2f res = a + b;
+
+  REQUIRE(res == vec2f(4.0f, 4.0f));
+}
+
+TEST_CASE("vec2f sub vec2f", "[vec2f]") {
+  vec2f a = vec2f(1.0f, 2.0f);
+  vec2f b = vec2f(3.0f, 2.0f);
+  vec2f res = a - b;
+
+  REQUIRE(res == vec2f(-2.0f, 0.0f));
+}
+
+TEST_CASE("vec2f mul vec2f", "[vec2f]") {
+  vec2f a = vec2f(1.0f, 2.0f);
+  vec2f b = vec2f(3.0f, 2.0f);
+  vec2f res = a * b;
+
+  REQUIRE(res == vec2f(3.0f, 4.0f));
+}
+
+TEST_CASE("vec2f div vec2f", "[vec2f]") {
+  vec2f a = vec2f(1.0f, 2.0f);
+  vec2f b = vec2f(3.0f, 2.0f);
+  vec2f res = a / b;
+
+  REQUIRE(res == vec2f(0.33333f, 1.0f));
+}
+
+// vec2f f32 operations
+
+TEST_CASE("vec2f add f32", "[vec2f]") {
+  vec2f a = vec2f(1.0f, 2.0f);
+  vec2f res = a + 2.0f;
+
+  REQUIRE(res == vec2f(3.0f, 4.0f));
+}
+
+TEST_CASE("vec2f sub f32", "[vec2f]") {
+  vec2f a = vec2f(1.0f, 2.0f);
+  vec2f res = a - 2.0f;
+
+  REQUIRE(res == vec2f(-1.0f, 0.0f));
+}
+
+TEST_CASE("vec2f mul f32", "[vec2f]") {
+  vec2f a = vec2f(1.0f, 2.0f);
+  vec2f res = a * 2.0f;
+
+  REQUIRE(res == vec2f(2.0f, 4.0f));
+}
+
+TEST_CASE("vec2f div f32", "[vec2f]") {
+  vec2f a = vec2f(1.0f, 2.0f);
+  vec2f res = a / 2.0f;
+
+  REQUIRE(res == vec2f(0.5f, 1.0f));
+}
+
+// vec2f vec2f assignment operators
+
+TEST_CASE("vec2f add assign vec2f", "[vec2f]") {
+  vec2f a = vec2f(1.0f, 2.0f);
+  a += vec2f(3.0f, 2.0f);
+
+  REQUIRE(a == vec2f(4.0f, 4.0f));
+}
+
+TEST_CASE("vec2f sub assign vec2f", "[vec2f]") {
+  vec2f a = vec2f(1.0f, 2.0f);
+  a -= vec2f(3.0f, 2.0f);
+
+  REQUIRE(a == vec2f(-2.0f, 0.0f));
+}
+
+TEST_CASE("vec2f mul assign vec2f", "[vec2f]") {
+  vec2f a = vec2f(1.0f, 2.0f);
+  a *= vec2f(3.0f, 2.0f);
+
+  REQUIRE(a == vec2f(3.0f, 4.0f));
+}
+
+TEST_CASE("vec2f div assign vec2f", "[vec2f]") {
+  vec2f a = vec2f(1.0f, 2.0f);
+  a /= vec2f(3.0f, 2.0f);
+
+  REQUIRE(a == vec2f(0.33333333f, 1.0f));
+}
+
+// vec2f float assignment operators
+
+TEST_CASE("vec2f add assign f32", "[vec2f]") {
+  vec2f a = vec2f(1.0f, 2.0f);
+  a += 2.0f;
+
+  REQUIRE(a == vec2f(3.0f, 4.0f));
+}
+
+TEST_CASE("vec2f sub assign f32", "[vec2f]") {
+  vec2f a = vec2f(1.0f, 2.0f);
+  a -= 2.0f;
+
+  REQUIRE(a == vec2f(-1.0f, 0.0f));
+}
+
+TEST_CASE("vec2f mul assign f32", "[vec2f]") {
+  vec2f a = vec2f(1.0f, 2.0f);
+  a *= 2.0f;
+
+  REQUIRE(a == vec2f(2.0f, 4.0f));
+}
+
+TEST_CASE("vec2f div assign f32", "[vec2f]") {
+  vec2f a = vec2f(1.0f, 2.0f);
+  a /= 2.0f;
+
+  REQUIRE(a == vec2f(0.5f, 1.0f));
+}
---
+++ tests/test_vec2i.cpp
@@ -0,0 +1,161 @@
+#include <catch.hpp>
+
+#include "vec2.h"
+
+TEST_CASE("vec2i constructor sets all fields", "[vec2i]") {
+  vec2i vec = vec2i(1, 2);
+
+  REQUIRE(vec.x == 1);
+  REQUIRE(vec.y == 2);
+}
+
+TEST_CASE("vec2i length", "[vec2i]") {
+  vec2i vec = vec2i(1, 2);
+  REQUIRE(APPROX_EQ(vec.length(), 2.236068f));
+}
+
+TEST_CASE("vec2i length_sqrt", "[vec2i]") {
+  vec2i vec = vec2i(1, 2);
+  REQUIRE(vec.length_sqrt() == 5);
+}
+
+TEST_CASE("vec2i dot", "[vec2i]") {
+  vec2i a = vec2i(1, 0);
+  vec2i b = vec2i(0, 1);
+  vec2i c = vec2i(1, 2);
+
+  REQUIRE(a.dot(b) == 0);
+  REQUIRE(c.dot(c) == c.length_sqrt());
+  REQUIRE(a.dot(c) == 1);
+  REQUIRE(b.dot(c) == 2);
+}
+
+TEST_CASE("vec2i equals operator", "[vec2i]") {
+  vec2i a = vec2i(2, -4);
+  vec2i b = vec2i(2, -4);
+  REQUIRE(a == b);
+}
+
+// vec2i vec2i operations
+
+TEST_CASE("vec2i add vec2i", "[vec2i]") {
+  vec2i a = vec2i(1, 2);
+  vec2i b = vec2i(3, 2);
+  vec2i res = a + b;
+
+  REQUIRE(res == vec2i(4, 4));
+}
+
+TEST_CASE("vec2i sub vec2i", "[vec2i]") {
+  vec2i a = vec2i(1, 2);
+  vec2i b = vec2i(3, 2);
+  vec2i res = a - b;
+
+  REQUIRE(res == vec2i(-2, 0));
+}
+
+TEST_CASE("vec2i mul vec2i", "[vec2i]") {
+  vec2i a = vec2i(1, 2);
+  vec2i b = vec2i(3, 2);
+  vec2i res = a * b;
+
+  REQUIRE(res == vec2i(3, 4));
+}
+
+TEST_CASE("vec2i div vec2i", "[vec2i]") {
+  vec2i a = vec2i(1, 2);
+  vec2i b = vec2i(3, 2);
+  vec2i res = a / b;
+
+  REQUIRE(res == vec2i(0, 1));
+}
+
+// vec2i f32 operations
+
+TEST_CASE("vec2i add i32", "[vec2i]") {
+  vec2i a = vec2i(1, 2);
+  vec2i res = a + 2;
+
+  REQUIRE(res == vec2i(3, 4));
+}
+
+TEST_CASE("vec2i sub i32", "[vec2i]") {
+  vec2i a = vec2i(1, 2);
+  vec2i res = a - 2;
+
+  REQUIRE(res == vec2i(-1, 0));
+}
+
+TEST_CASE("vec2i mul i32", "[vec2i]") {
+  vec2i a = vec2i(1, 2);
+  vec2i res = a * 2;
+
+  REQUIRE(res == vec2i(2, 4));
+}
+
+TEST_CASE("vec2i div i32", "[vec2i]") {
+  vec2i a = vec2i(2, 4);
+  vec2i res = a / 2;
+
+  REQUIRE(res == vec2i(1, 2));
+}
+
+// vec2i vec2i assignment operators
+
+TEST_CASE("vec2i add assign vec2i", "[vec2i]") {
+  vec2i a = vec2i(1, 2);
+  a += vec2i(3, 2);
+
+  REQUIRE(a == vec2i(4, 4));
+}
+
+TEST_CASE("vec2i sub assign vec2i", "[vec2i]") {
+  vec2i a = vec2i(1, 2);
+  a -= vec2i(3, 2);
+
+  REQUIRE(a == vec2i(-2, 0));
+}
+
+TEST_CASE("vec2i mul assign vec2i", "[vec2i]") {
+  vec2i a = vec2i(1, 2);
+  a *= vec2i(3, 2);
+
+  REQUIRE(a == vec2i(3, 4));
+}
+
+TEST_CASE("vec2i div assign vec2i", "[vec2i]") {
+  vec2i a = vec2i(1, 2);
+  a /= vec2i(3, 2);
+
+  REQUIRE(a == vec2i(0, 1));
+}
+
+// vec2i float assignment operators
+
+TEST_CASE("vec2i add assign i32", "[vec2i]") {
+  vec2i a = vec2i(1, 2);
+  a += 2;
+
+  REQUIRE(a == vec2i(3, 4));
+}
+
+TEST_CASE("vec2i sub assign i32", "[vec2i]") {
+  vec2i a = vec2i(1, 2);
+  a -= 2;
+
+  REQUIRE(a == vec2i(-1, 0));
+}
+
+TEST_CASE("vec2i mul assign i32", "[vec2i]") {
+  vec2i a = vec2i(1, 2);
+  a *= 2;
+
+  REQUIRE(a == vec2i(2, 4));
+}
+
+TEST_CASE("vec2i div assign i32", "[vec2i]") {
+  vec2i a = vec2i(1, 2);
+  a /= 2;
+
+  REQUIRE(a == vec2i(0, 1));
+}
