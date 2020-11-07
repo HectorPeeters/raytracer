b4b15470a3631213475d14207b343e925c03d44a
Hector Peeters <hector.peeters@gmail.com>
2020-11-07 12:46:27 +0100

    Add vec4 type

---
+++ include/vec4.h
@@ -0,0 +1,182 @@
+#pragma once
+
+#include "core.h"
+
+template <typename T> class vec4 {
+public:
+  inline vec4(T x, T y, T z, T w) : x(x), y(y), z(z), w(w) {}
+  ~vec4() {}
+
+  f32 length() { return sqrtf(x * x + y * y + z * z + w * w); }
+
+  f32 length_sqrt() { return x * x + y * y + z * z + w * w; }
+
+  f32 distance(const vec4<T> &other) {
+    return sqrtf((x - other.x) * (x - other.x) + (y - other.y) * (y - other.y) +
+                 (z - other.z) * (z - other.z) + (w - other.w) * (w - other.w));
+  }
+
+  vec4<T> normalized() {
+    T one_over_length = 1 / sqrtf(x * x + y * y + z * z + w * w);
+
+    return vec4<T>{
+        x * one_over_length,
+        y * one_over_length,
+        z * one_over_length,
+        w * one_over_length,
+    };
+  }
+
+  void normalize() {
+    T one_over_length = 1 / sqrtf(x * x + y * y + z * z + w * w);
+
+    x *= one_over_length;
+    y *= one_over_length;
+    z *= one_over_length;
+    w *= one_over_length;
+  }
+
+  T dot(const vec4<T> &other) {
+    return x * other.x + y * other.y + z * other.z + w * other.w;
+  }
+
+  vec4<T> operator+(const vec4<T> &right) {
+    return vec4<T>{
+        x + right.x,
+        y + right.y,
+        z + right.z,
+        w + right.w
+    };
+  }
+
+  vec4<T> operator-(const vec4<T> &right) {
+    return vec4<T>{
+        x - right.x,
+        y - right.y,
+        z - right.z,
+        w - right.w,
+    };
+  }
+
+  vec4<T> operator*(const vec4<T> &right) {
+    return vec4<T>{
+        x * right.x,
+        y * right.y,
+        z * right.z,
+        w * right.w,
+    };
+  }
+
+  vec4<T> operator/(const vec4<T> &right) {
+    return vec4<T>{
+        x / right.x,
+        y / right.y,
+        z / right.z,
+        w / right.w,
+    };
+  }
+
+  void operator+=(const vec4<T> &right) {
+    x += right.x;
+    y += right.y;
+    z += right.z;
+    w += right.w;
+  }
+
+  void operator-=(const vec4<T> &right) {
+    x -= right.x;
+    y -= right.y;
+    z -= right.z;
+    w -= right.w;
+  }
+
+  void operator*=(const vec4<T> &right) {
+    x *= right.x;
+    y *= right.y;
+    z *= right.z;
+    w *= right.w;
+  }
+
+  void operator/=(const vec4<T> &right) {
+    x /= right.x;
+    y /= right.y;
+    z /= right.z;
+    w /= right.w;
+  }
+
+  vec4<T> operator+(T right) {
+    return vec4<T>{
+        x + right,
+        y + right,
+        z + right,
+        w + right,
+    };
+  }
+
+  vec4<T> operator-(T right) {
+    return vec4<T>{
+        x - right,
+        y - right,
+        z - right,
+        w - right,
+    };
+  }
+
+  vec4<T> operator*(T right) {
+    return vec4<T>{
+        x * right,
+        y * right,
+        z * right,
+        w * right,
+    };
+  }
+
+  vec4<T> operator/(T right) {
+    return vec4<T>{
+        x / right,
+        y / right,
+        z / right,
+        w / right,
+    };
+  }
+
+  void operator+=(T right) {
+    x += right;
+    y += right;
+    z += right;
+    w += right;
+  }
+
+  void operator-=(T right) {
+    x -= right;
+    y -= right;
+    z -= right;
+    w -= right;
+  }
+
+  void operator*=(T right) {
+    x *= right;
+    y *= right;
+    z *= right;
+    w *= right;
+  }
+
+  void operator/=(T right) {
+    x /= right;
+    y /= right;
+    z /= right;
+    w /= right;
+  }
+
+public:
+  T x, y, z, w;
+};
+
+template <typename T>
+bool operator==(const vec4<T> &left, const vec4<T> &right) {
+  return APPROX_EQ(left.x, right.x) && APPROX_EQ(left.y, right.y) &&
+         APPROX_EQ(left.z, right.z) && APPROX_EQ(left.w, right.w);
+}
+
+typedef vec4<f32> vec4f;
+typedef vec4<i32> vec4i;
